a367c9f7b6d8b695757ddbd4ca5be298289fada3
Francis Rhys-Jones <francis@rhys-jones.com>
2019-08-20 12:13:35 +0100

    Removing timeout for application completion

--- application/src/main/scala/com/gu/deskcomexporttool/DeskComExportToolApp.scala
+++ application/src/main/scala/com/gu/deskcomexporttool/DeskComExportToolApp.scala
@@ -5,5 +5,5 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object DeskComExportToolApp extends App {
-  System.exit(Await.result(CommandLineRunner(Exporter(S3Service(), DeskComClientFactory())).run(args), 1 hour))
+  System.exit(Await.result(CommandLineRunner(Exporter(S3Service(), DeskComClientFactory())).run(args), Duration.Inf))
 }
